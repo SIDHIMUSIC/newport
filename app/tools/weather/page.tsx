358efa226578680a95107ff70c630d6b43b0704f
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-15 18:53:28 +0530

    Update page.tsx

--- app/tools/weather/page.tsx
+++ app/tools/weather/page.tsx
@@ -1,23 +1,6 @@
-"use client"
+import Weather from "@/components/Weather"
 
-import { useState } from "react"
-
-export default function WeatherTool(){
-
-  const [city,setCity] = useState("")
-  const [data,setData] = useState<any>(null)
-
-  const getWeather = async ()=>{
-
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=Y5ece445b397ff6b757c3e2e92edc8a15&units=metric`
-    )
-
-    const result = await res.json()
-
-    setData(result)
-
-  }
+export default function WeatherPage(){
 
   return(
 
@@ -25,44 +8,12 @@ export default function WeatherTool(){
       style={{
         height:"100vh",
         display:"flex",
-        flexDirection:"column",
         alignItems:"center",
         justifyContent:"center"
       }}
     >
 
-      <h1 style={{fontSize:"40px",marginBottom:"30px"}}>
-        Weather Tool
-      </h1>
-
-      <input
-        placeholder="Enter City"
-        value={city}
-        onChange={(e)=>setCity(e.target.value)}
-        style={{padding:"10px",fontSize:"16px"}}
-      />
-
-      <button
-        onClick={getWeather}
-        style={{
-          marginTop:"10px",
-          padding:"10px 20px"
-        }}
-      >
-        Get Weather
-      </button>
-
-      {data && (
-
-        <div style={{marginTop:"20px"}}>
-
-          <h2>{data.name}</h2>
-          <p>{data.main.temp} °C</p>
-          <p>{data.weather[0].main}</p>
-
-        </div>
-
-      )}
+      <Weather/>
 
     </div>
 
