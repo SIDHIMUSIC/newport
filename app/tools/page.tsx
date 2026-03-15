f30ed05b93e3c049aaae67959ee266743bef7ede
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-15 19:14:49 +0530

    Update page.tsx

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,106 +1,43 @@
-"use client"
+import Link from "next/link"
 
-import { useState } from "react"
-
-export default function Weather(){
-
-  const [city,setCity] = useState("")
-  const [weather,setWeather] = useState<any>(null)
-  const [error,setError] = useState("")
-
-  const getWeather = async () => {
-
-    try{
-
-      setError("")
-      setWeather(null)
-
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5ece445b397ff6b757c3e2e92edc8a15&units=metric`
-      )
-
-      const data = await res.json()
-
-      if(data.cod !== 200){
-        setError("City not found")
-        return
-      }
-
-      setWeather(data)
-
-    }catch{
-      setError("Weather fetch failed")
-    }
-
-  }
+export default function Tools(){
 
   return(
 
-    <div
-      style={{
-        background:"#1e293b",
-        padding:"30px",
-        borderRadius:"15px",
-        color:"white",
-        width:"350px",
-        textAlign:"center"
-      }}
-    >
+    <div style={{padding:"40px"}}>
 
-      <h2 style={{fontSize:"28px",marginBottom:"20px"}}>
-        Weather Tool
-      </h2>
+      <h1 style={{fontSize:"40px"}}>
+        Developer Tools
+      </h1>
 
-      <input
-        placeholder="Enter city"
-        value={city}
-        onChange={(e)=>setCity(e.target.value)}
-        style={{
-          padding:"10px",
-          width:"70%",
-          borderRadius:"8px",
-          border:"none"
-        }}
-      />
+      <div style={{
+        marginTop:"30px",
+        display:"flex",
+        gap:"20px"
+      }}>
 
-      <button
-        onClick={getWeather}
-        style={{
-          marginLeft:"10px",
-          padding:"10px",
-          borderRadius:"8px",
-          border:"none",
-          background:"#38bdf8",
-          color:"black",
-          cursor:"pointer"
-        }}
-      >
-        Search
-      </button>
+        <Link href="/tools/calculator">
+          <button style={{
+            padding:"20px",
+            fontSize:"18px"
+          }}>
+            Calculator
+          </button>
+        </Link>
 
-      {error && (
-        <p style={{color:"red",marginTop:"15px"}}>
-          {error}
-        </p>
-      )}
+        <Link href="/tools/weather">
+          <button style={{
+            padding:"20px",
+            fontSize:"18px"
+          }}>
+            Weather
+          </button>
+        </Link>
 
-      {weather && (
-
-        <div style={{marginTop:"20px"}}>
-
-          <h3>{weather.name}</h3>
-
-          <p>🌡 Temperature: {weather.main.temp} °C</p>
-
-          <p>☁ Weather: {weather.weather[0].main}</p>
-
-          <p>💧 Humidity: {weather.main.humidity}%</p>
-
-        </div>
-
-      )}
+      </div>
 
     </div>
 
   )
+
 }
