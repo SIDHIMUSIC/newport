78c4c3d95494ac876620b3bc910ab9e7321fd7b8
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-15 18:07:32 +0530

    Update page.tsx

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -4,9 +4,17 @@ export default function ToolsPage(){
 
   return(
 
-    <div style={{padding:"40px"}}>
+    <div
+      style={{
+        height:"100vh",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center"
+      }}
+    >
 
-      <h1 style={{fontSize:"36px"}}>
+      <h1 style={{fontSize:"40px",marginBottom:"30px"}}>
         Developer Tools
       </h1>
 
